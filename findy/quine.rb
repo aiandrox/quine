08833633ebc5385e421f5fc98e44415be6cfa601
aiandrox <aiandrox7@gmail.com>
2024-05-20 10:20:21 +0900

    動くようになった

---
+++ findy/quine.rb
@@ -0,0 +1,15 @@
+           eval$s=
+       %w'b="BAhsK3oA+
+     AMAAAAAAAAAAAAAAAAA             8P8HAAAAAAAAAAAAA   AAAgP/                                    /A4D/
+   //gBAAA         A4AMAAP           wB/AHw/x8/AAAAAHw   AAMAPA                                    H4Afg
+  AAgAMA             AMCHAz          j4PAAf                        wA8                            A/PD/    B/z         /8Y
+  Of/wP  gA/g          BgB/+         /8P/P3              z48T8A    eAD/f/DDD3784Ye       fH/4AgA/g/w9++M   CPD/jg     /4EPAP
+ AB/P/BDx/w8           QEf+B         /wA4Af              gB8A+O    EDPn7wA/4B/AP+AfA    DAD98wIf/f4AfAP7    //wB+    AOCHD/
+ jg/w/wAQD+             Pz6A         DwD44AEP4OcBPwA     AAAAAA    AAAAAA     AAAAA8   AM=";n=    Marsha    l.load  (b.unp
+ ack("m"               )[0])         ;e="eval$s=%w"<     <39<<(    $s*3)      ;o="";   j=-1;       0.upt     o(15*125-1){
+  |i|;o                <<((n         [i]==1)?e[j+=1]     :32);o    <<((i       %125=   =(124       ))?10      :"");};o[-
+  16,6]=             ""<<39          <<".jo              in";pu    ts(o)       #b="B   AhsK3o     A+AMAA       AAAAAAAA
+   AAAAAAA8       P8HAAAAA           AAAAAA              AAAAAg    P//A4       D///g    BAAAA4AMAAPwB/AH        w/x8/A
+     AAAAHwAAMAPAH4AfgAAgAMA         AMCHAz              j4PAAf    wA8A/       PD/B/     z/8YOf/wPgA/gBg        B/+/8
+        P/P3z48T8AeAD   /f/DD         D3784               YefH/     4AgA       /g/w         9++MCP  D/jg       /4EPAP
+                                                                                                              '.join
