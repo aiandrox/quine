54588ebae6ca6625fbd0013a8db11cb86406411d
aiandrox <aiandrox7@gmail.com>
2024-05-21 19:47:45 +0900

    少し短くした

--- findy/quine.rb
+++ findy/quine.rb
@@ -1,21 +1,21 @@
-             eval$s=%w'a=
-         proc{|b|Marshal.load
-       (b.unpack("m")[0])};n=a.c                  all("BAhsKwHUAOD/AQAA     AAAAAAA                                                 AAAAAAA
-     AAAAAAgP//         BwAAAAAAA                 AAAAAAAAAAAAAAA+P//Dw     DA//8Hf                                                 wAAAAAA
-    AH8AAACA               /wH8BwDw               ///BHwAAAAAAwB8AAADwD     wD4BwD8                                                 /3/wBwA
-   AAAAA8A                   cAAAD+A              AD4AwA                                                                            /AAAAAA
-  AAAAAA/   AEAAM             AfHwD8              AMAPAA                    B/4Pv/A      PD/f/ wB/PH7AwB+APA            DAMAf+P//AP//H/6AP/   7/AIAfA         PwAAPAH
-  /v9/4P //B3/wh               /8PAOA             HAP//B                    /yBf+Af      /AP/wT/+4H8AAPgBwP//A        X/gD/AHf4B/4N8f8A8AAH    8A8P9/w       B/4A/zB
- D8Af8P8D/AEAwA8               A/AAA8             Af+AH/                    wA/AH+H      8A/gAA/AEAPwAA/IE/wB/8      Af4B/B8A/wHAPwDADwAAf+     AP8Af/w     X8A/gOA
- /4f/HwDwAwDAH                 /gD/IH             //x+AfwCA////HwD8AA       DwB/4Af      4D//wfwD      wAA///jA     wB/AAD4A      3+APwD88Q     P4AwAA+A   8gAAAAA
- AAAAAAAAAA                    AAIA/A             AAAAAAAAAAAAAAAAAAA       AAAAAPA      HAP////        //////P     w==");c        =a.call(      "BAhbFG8 6ClJhbm
-  dlCDoJZX                    hjbEY6C             mJlZ2luaRI6CGVuZGkC       ZAFvOwA      IOwZGOw        dpAvQBO     whpAgg          CbzsACD       sGRjsHaQKUAjsI
-  aQKoAm8                     7AAg7B              kY7B2k                    CPgM7CG      kCUgNvO        wAIOwZG     OwdpAu          MDOwhpA        vwDbzsACDsGR
-   jsHaQKK                  BDsIaQK               cBG87A                    Ag7BkY7      B2kCMAU        7CGkCQQ     VvOwAIO        wZGOwdpA         tcFOwhpAuYF
-    bzsACDsGR             jsHaQJ8B                jsIaQK                    LBm87AA      g7BkY7B        2kCIQc7     CGkCMAdvO     wAIOwZGOw          dpAsYHOwh
-     pAtUHbzsACDs    GRjsHaQJrCDsIa               QJ6CG8                    7AAg7Bk      Y7B2kCE        Ak7CGkC      IAlvOwAIOwZGOwdpAroJOw          hpAsoJbz
-       sACDsGRjsHaQJfCjsIaQJtCg==").f             lat_ma                    p(&:to_      a);e="e        val$s=%        w"+39.chr+($s*3);o="         ";j=-1;;
-          0.upto(3485){|i|;t   =(c.i              nclude?                    (i)?34:      0);o<<2        7.chr+"            ["+t.to   _s+"m"         ;o<<(n[
-               i]==1?e[j         +                                                                                                                 =1]:32)
-                                                                                                                                                  ;o<<(i%
-166==165?10:"");o<<27.chr+"[0m";};o[-102,20]=""+39.chr+".join";puts(o)                                                                                     '.join
+             eval$s=%w'l=
+         39.chr;m=27.chr;a=pr
+       oc{|b|Marshal.load(b.unpa                  ck("m")[0])};n=a.call     ("BAhsK                                                 wHUAOD/
+     AQAAAAAAAA         AAAAAAAAA                 AAAAAgP//BwAAAAAAAAAA     AAAAAAA                                                 AAAAA+P
+    //DwDA//               8HfwAAAA               AAAH8AAACA/wH8BwDw///     BHwAAAA                                                 AAwB8AA
+   ADwDwD4                   BwD8/3/              wBwAAA                                                                            AAA8AcA
+  AAD+AAD   4AwA/             AAAAAA              AAAAAA                    /AEAAMA      fHwD8 AMAPAAB/4Pv/A            PD/f/wB/PH7AwB+APAD   AMAf+P/         /AP//H/
+  6AP/7/ AIAfAPw               AAPAH/             v9/4P/                    /B3/wh/      8PAOAHAP//B/yBf+Af/AP        /wT/+4H8AAPgBwP//AX/g    D/AHf4B       /4N8f8A
+ 8AAH8A8P9/wB/4A               /zBD8A             f8P8D/                    AEAwA8A      /AAA8Af+AH/wA/AH+H8A/g      AA/AEAPwAA/IE/wB/8Af4B     /B8A/wH     APwDADw
+ AAf+AP8Af/wX8                 A/gOA/             4f/HwDwAwDAH/gD/IH/       /x+AfwC      A////HwD      8AADwB/4     Af4D//wf      wDwAA///j     AwA/AAD8   gT/AHwD
+ ++AH8AQAA+                    A8gAAA             AAAAAAAAAAAAAAIA/AA       AAAAAAA      AAAAAAA        AAAAAAA     AAPAHAP        ////////      //Bw==") ;c=a.ca
+  ll("BAhb                    FG86ClJ             hbmdlCDoJZXhjbEY6Cm       JlZ2lua      RI6CGVu        ZGkCZAF     vOwAIO          wZGOwdp       AvQBOwhpAggCbz
+  sACDsGR                     jsHaQK              UAjsIa                    QKoAm87      AAg7BkY        7B2kCPg     M7CGkC          UgNvOwA        IOwZGOwdpAuM
+   DOwhpAv                  wDbzsAC               DsGRjs                    HaQKKBD      sIaQKcB        G87AAg7     BkY7B2k        CMAU7CGk         CQQVvOwAIOw
+    ZGOwdpAtc             FOwhpAuY                FbzsAC                    DsGRjsH      aQJ8Bjs        IaQKLBm     87AAg7BkY     7B2kCIQc7          CGkCMAdvO
+     wAIOwZGOwdpA    sYHOwhpAtUHbzs               ACDsGR                    jsHaQJr      CDsIaQJ        6CG87AA      g7BkY7B2kCEAk7CGkCIAlv          OwAIOwZG
+       OwdpAroJOwhpAsoJbzsACDsGRjsHaQ             JfCjsI                    aQJtCg=      =").fla        t_map(&        :to_a);e="eval$s=%w"         +l+($s*2
+          );o="";j=-1;;0.upt   o(348              5){|i|                    ;t=(c.i      nclude?        (i)?34:            0);o<<m   +"["+t         .to_s+"
+               m";o<<(n[         i                                                                                                                 ]==1?e[
+                                                                                                                                                  j+=1]:3
+2);o<<(i%166==165?10:"");o<<m+"[0m";};o[-901,9]=l+".join";puts(o)#'.join
