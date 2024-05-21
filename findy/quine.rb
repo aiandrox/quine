b7e53ab9766e2611e46287ccab2bc4140e03ef8e
aiandrox <aiandrox7@gmail.com>
2024-05-21 18:26:09 +0900

    最後のあたり以外は取りあえず動く

--- findy/quine.rb
+++ findy/quine.rb
@@ -1,15 +1,21 @@
-           eval$s=
-       %w'b="BAhsK3oA+
-     AMAAAAAAAAAAAAAAAAA             8P8HAAAAAAAAAAAAA   AAAgP/                                    /A4D/
-   //gBAAA         A4AMAAP           wB/AHw/x8/AAAAAHw   AAMAPA                                    H4Afg
-  AAgAMA             AMCHAz          j4PAAf                        wA8                            A/PD/    B/z         /8Y
-  Of/wP  gA/g          BgB/+         /8P/P3              z48T8A    eAD/f/DDD3784Ye       fH/4AgA/g/w9++M   CPD/jg     /4EPAP
- AB/P/BDx/w8           QEf+B         /wA4Af              gB8A+O    EDPn7wA/4B/AP+AfA    DAD98wIf/f4AfAP7    //wB+    AOCHD/
- jg/w/wAQD+             Pz6A         DwD44AEP4OcBPwA     AAAAAA    AAAAAA     AAAAA8   AM=";n=    Marsha    l.load  (b.unp
- ack("m"               )[0])         ;e="eval$s=%w"<     <39<<(    $s*3)      ;o="";   j=-1;       0.upt     o(15*125-1){
-  |i|;o                <<((n         [i]==1)?e[j+=1]     :32);o    <<((i       %125=   =(124       ))?10      :"");};o[-
-  16,6]=             ""<<39          <<".jo              in";pu    ts(o)       #b="B   AhsK3o     A+AMAA       AAAAAAAA
-   AAAAAAA8       P8HAAAAA           AAAAAA              AAAAAg    P//A4       D///g    BAAAA4AMAAPwB/AH        w/x8/A
-     AAAAHwAAMAPAH4AfgAAgAMA         AMCHAz              j4PAAf    wA8A/       PD/B/     z/8YOf/wPgA/gBg        B/+/8
-        P/P3z48T8AeAD   /f/DD         D3784               YefH/     4AgA       /g/w         9++MCP  D/jg       /4EPAP
-                                                                                                              '.join
+             eval$s=%w'a=
+         proc{|b|Marshal.load
+       (b.unpack("m")[0])};n=a.c                  all("BAhsKwHUAOD/AQAA     AAAAAAA                                                 AAAAAAA
+     AAAAAAgP//         BwAAAAAAA                 AAAAAAAAAAAAAAA+P//Dw     DA//8Hf                                                 wAAAAAA
+    AH8AAACA               /wH8BwDw               ///BHwAAAAAAwB8AAADwD     wD4BwD8                                                 /3/wBwA
+   AAAAA8A                   cAAAD+A              AD4AwA                                                                            /AAAAAA
+  AAAAAA/   AEAAM             AfHwD8              AMAPAA                    B/4Pv/A      PD/f/ wB/PH7AwB+APA            DAMAf+P//AP//H/6AP/   7/AIAfA         PwAAPAH
+  /v9/4P //B3/wh               /8PAOA             HAP//B                    /yBf+Af      /AP/wT/+4H8AAPgBwP//A        X/gD/AHf4B/4N8f8A8AAH    8A8P9/w       B/4A/zB
+ D8Af8P8D/AEAwA8               A/AAA8             Af+AH/                    wA/AH+H      8A/gAA/AEAPwAA/IE/wB/8      Af4B/B8A/wHAPwDADwAAf+     AP8Af/w     X8A/gOA
+ /4f/HwDwAwDAH                 /gD/IH             //x+AfwCA////HwD8AA       DwB/4Af      4D//wfwD      wAA///jA     wB/AAD4A      3+APwD88Q     P4AwAA+A   8gAAAAA
+ AAAAAAAAAA                    AAIA/A             AAAAAAAAAAAAAAAAAAA       AAAAAPA      HAP////        //////P     w==");c        =a.call(      "BAhbFG8 6ClJhbm
+  dlCDoJZX                    hjbEY6C             mJlZ2luaRI6CGVuZGkC       ZAFvOwA      IOwZGOw        dpAvQBO     whpAgg          CbzsACD       sGRjsHaQKUAjsI
+  aQKoAm8                     7AAg7B              kY7B2k                    CPgM7CG      kCUgNvO        wAIOwZG     OwdpAu          MDOwhpA        vwDbzsACDsGR
+   jsHaQKK                  BDsIaQK               cBG87A                    Ag7BkY7      B2kCMAU        7CGkCQQ     VvOwAIO        wZGOwdpA         tcFOwhpAuYF
+    bzsACDsGR             jsHaQJ8B                jsIaQK                    LBm87AA      g7BkY7B        2kCIQc7     CGkCMAdvO     wAIOwZGOw          dpAsYHOwh
+     pAtUHbzsACDs    GRjsHaQJrCDsIa               QJ6CG8                    7AAg7Bk      Y7B2kCE        Ak7CGkC      IAlvOwAIOwZGOwdpAroJOw          hpAsoJbz
+       sACDsGRjsHaQJfCjsIaQJtCg==").f             lat_ma                    p(&:to_      a);e="e        val$s=%        w"+39.chr+($s*3);o="         ";j=-1;;
+          0.upto(3485){|i|;t   =(c.i              nclude?                    (i)?34:      0);o<<2        7.chr+"            ["+t.to   _s+"m"         ;o<<(n[
+               i]==1?e[j         +                                                                                                                 =1]:32)
+                                                                                                                                                  ;o<<(i%
+166==165?10:"");o<<27.chr+"[0m";};o[-102,20]=""+39.chr+".join";puts(o)                                                                                     '.join
